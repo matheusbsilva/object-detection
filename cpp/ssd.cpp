0b77e7c114fa22806b82a5449bde5580e0c166f7
matheusbsilva <matheusbattista@hotmail.com>
2019-07-07 23:46:44 +0000

    Change resolution

--- cpp/ssd.cpp
+++ cpp/ssd.cpp
@@ -5,20 +5,21 @@
 using namespace std;
 using namespace cv;
 using namespace cv::dnn;
+using namespace std::chrono;
 
 
 int main() {
-    int inpWidth = 300;
-    int inpHeight = 300;
+    int inpWidth = 200;
+    int inpHeight = 200;
     float conf_threshold = 0.4;
 
-    string modelcfg = "ssd/ssd_mobilenet_v2_coco_2018_03_29.pbtxt";
-    string modelweights = "ssd/frozen_inference_graph.pb";
+    string modelcfg = "../ssd/ssd_mobilenet_v2_coco_2018_03_29.pbtxt";
+    string modelweights = "../ssd/frozen_inference_graph.pb";
 
     Net net = readNetFromTensorflow(modelweights, modelcfg);
 
     VideoCapture cap;
-    cap.open(2);
+    cap.open(0);
 
     Mat frame;
 
@@ -29,10 +30,17 @@ int main() {
         cap.read(frame);
         resize(frame, frame, Size(inpWidth, inpHeight));
 
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
         blobFromImage(frame, blob, 1.0, Size(inpWidth, inpHeight), Scalar(0,0,0), true, false);
 
         net.setInput(blob);
         outs = net.forward("detection_out");
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>( t2 - t1 ).count();
+
+	cout << "Duration: " << duration << endl;
+   
 
         int detections = outs.size[2];
 
@@ -42,7 +50,6 @@ int main() {
 
             float confidence = (float)outs.at<float>(id_conf);
 
-            cout << "Conf: " << confidence << endl; 
             if(confidence > conf_threshold) {
                 int id_xlb[4] = {0, 0, i, 3};
                 int id_ylb[4] = {0, 0, i, 4};
@@ -53,8 +60,6 @@ int main() {
                 int yLeftBottom = outs.at<float>(id_ylb) * frame.rows;
                 int xRightTop = outs.at<float>(id_xrt) * frame.cols;
                 int yRightTop = outs.at<float>(id_yrt) * frame.rows;
-
-                cout << xLeftBottom << endl;
 
                 rectangle(frame, Point(xLeftBottom, yLeftBottom), 
                           Point(xRightTop, yRightTop), Scalar(0, 255, 0));
